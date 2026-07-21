16d050ab9b6bbc0314cf48419a6f97c811f54d53
Denis V <5462781+denisvmedia@users.noreply.github.com>
2026-07-21 07:49:03 +0200

    Exercise Ptah action PR comment

--- models/models.go
+++ models/models.go
@@ -4,4 +4,7 @@ package models
 type User struct {
 	//migrator:schema:field name="id" type="INTEGER" primary="true"
 	ID int
+
+	//migrator:schema:field name="email" type="TEXT" not_null="true" unique="true"
+	Email string
 }
